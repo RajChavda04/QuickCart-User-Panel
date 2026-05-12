c0ed09d8dc761748136a2b1a7cc9e41db0ea154a
Raj Chavda <rajchavda463@gmail.com>
2026-05-12 15:58:11 +0530

    config file upated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -42,48 +42,48 @@ const handleSearch = (e) => {
   return (
     <>
 
-      <div class="header sticky-active">
+      <div className="header sticky-active">
 
-        <div class="header-middle">
-          <div class="container">
-            <div class="header-middle-inner">
-              <div class="header-middle-left">
-                <div class="header-logo d-lg-block">
+        <div className="header-middle">
+          <div className="container">
+            <div className="header-middle-inner">
+              <div className="header-middle-left">
+                <div className="header-logo d-lg-block">
                   <a href="index.html">
                     <img src="assets/img/logo/QuickCart.png" alt="Logo" />
                   </a>
                 </div>
-                <div class="category-form-wrap">
+                <div className="category-form-wrap">
 
-                  <div class="header-form" >
-                    <input class="form-control" type="text" name="search"  value={query}
+                  <div className="header-form" >
+                    <input className="form-control" type="text" name="search"  value={query}
                   onChange={(e) => setQuery(e.target.value)}
                   onKeyDown={handleSearch} placeholder="Search here..." />
-                    <button class="submit rr-primary-btn">Search here</button>
+                    <button className="submit rr-primary-btn">Search here</button>
                   </div>
                 </div>
               </div>
-              <div class="header-middle-right">
+              <div className="header-middle-right">
              
-                <ul class="contact-item-list">
+                <ul className="contact-item-list">
 
                   <li>
-                    <Link to="/Wish" class="icon">
-                      <i class="fa-sharp fa-regular fa-heart"></i>
+                    <Link to="/Wish" className="icon">
+                      <i className="fa-sharp fa-regular fa-heart"></i>
                     </Link>
                   </li>
                   <li>
                  
-                    <Link to="/Cart" class="icon" >
-                      <i class="fa-light fa-bag-shopping"></i>
+                    <Link to="/Cart" className="icon" >
+                      <i className="fa-light fa-bag-shopping"></i>
                       <span>2</span>
                     </Link>
                        
                   </li>
                   <li>
                  
-                    <Link to="/Order" class="icon" >
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-seam" viewBox="0 0 16 16">
+                    <Link to="/Order" className="icon" >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-box-seam" viewBox="0 0 16 16">
                     <path d="M8.186 1.113a.5.5 0 0 0-.372 0L1.846 3.5l2.404.961L10.404 2zm3.564 1.426L5.596 5 8 5.961 14.154 3.5zm3.25 1.7-6.5 2.6v7.922l6.5-2.6V4.24zM7.5 14.762V6.838L1 4.239v7.923zM7.443.184a1.5 1.5 0 0 1 1.114 0l7.129 2.852A.5.5 0 0 1 16 3.5v8.662a1 1 0 0 1-.629.928l-7.185 2.874a.5.5 0 0 1-.372 0L.63 13.09a1 1 0 0 1-.63-.928V3.5a.5.5 0 0 1 .314-.464z"/>
                   </svg>
                     </Link>
@@ -92,10 +92,10 @@ const handleSearch = (e) => {
 
                   <li>
                  
-                    <Link to="/Userprofile" class="icon"  >
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <Link to="/Userprofile" className="icon"  >
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
                         <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
-                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                        <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
                       </svg>
                     </Link>
                
@@ -107,26 +107,26 @@ const handleSearch = (e) => {
             </div>
           </div>
         </div>
-        <div class="primary-header">
-          <div class="container">
-            <div class="primary-header-inner">
-              <div class="header-logo mobile-logo">
+        <div className="primary-header">
+          <div className="container">
+            <div className="primary-header-inner">
+              <div className="header-logo mobile-logo">
                 <a href="index.html">
                   <img src="assets/img/logo/logo-1.png" alt="Logo" />
                 </a>
               </div>
-              <div class="header-menu-wrap">
-                <div class="mobile-menu-items">
+              <div className="header-menu-wrap">
+                <div className="mobile-menu-items">
                   <ul>
-                    <li class="menu-item-has-children active">
+                    <li className="menu-item-has-children active">
                       <Link to="/">Home</Link>
 
                     </li>
-                    <li class="menu-item-has-children active">
+                    <li className="menu-item-has-children active">
                       <Link to="/Shope">Shop</Link>
 
                     </li>
-                    <li class="menu-item-has-children">
+                    <li className="menu-item-has-children">
                       <a href="/">Category</a>
 
                       <ul>
@@ -138,7 +138,7 @@ const handleSearch = (e) => {
 
                     </li>
 
-                    <li class="menu-item-has-children">
+                    <li className="menu-item-has-children">
                       <a href="/">Pages</a>
                       <ul>
                         {/* <li><Link to="about.html">About</Link></li> */}
@@ -154,11 +154,11 @@ const handleSearch = (e) => {
                 </div>
               </div>
 
-              <div class="header-right-wrap">
-                <div class="header-right">
+              <div className="header-right-wrap">
+                <div className="header-right">
                   <span>Get 40% Discount Now <span>Sale</span></span>
-                  <div class="header-right-item">
-                    <a href="javascript:void(0)" class="mobile-side-menu-toggle"><i class="fa-sharp fa-solid fa-bars"></i></a>
+                  <div className="header-right-item">
+                    <a href="javascript:void(0)" className="mobile-side-menu-toggle"><i className="fa-sharp fa-solid fa-bars"></i></a>
                   </div>
                 </div>
 
@@ -170,24 +170,24 @@ const handleSearch = (e) => {
       </div>
 
       <div id="popup-search-box">
-        <div class="box-inner-wrap d-flex align-items-center">
+        <div className="box-inner-wrap d-flex align-items-center">
           <form id="form" action="#" method="get" role="search">
             <input id="popup-search" type="text" name="search"  placeholder="Type keywords here..." />
           </form>
-          <div class="search-close"><i class="fa-sharp fa-regular fa-xmark"></i></div>
+          <div className="search-close"><i className="fa-sharp fa-regular fa-xmark"></i></div>
         </div>
       </div>
-      <div class="mobile-side-menu">
-        <div class="side-menu-content">
-          <div class="side-menu-head">
+      <div className="mobile-side-menu">
+        <div className="side-menu-content">
+          <div className="side-menu-head">
             <a href='index.html'><img src="assets/img/logo/logo-1.png" alt="logo" /></a>
-            <button class="mobile-side-menu-close"><i class="fa-regular fa-xmark"></i></button>
+            <button className="mobile-side-menu-close"><i className="fa-regular fa-xmark"></i></button>
           </div>
-          <div class="side-menu-wrap"></div>
-          <ul class="side-menu-list">
-            <li><i class="fa-light fa-location-dot"></i>Address : <span>Amsterdam, 109-74</span></li>
-            <li><i class="fa-light fa-phone"></i>Phone : <a href="tel:+01569896654">+01 569 896 654</a></li>
-            <li><i class="fa-light fa-envelope"></i>Email : <a href="mailto:info@example.com">info@example.com</a>
+          <div className="side-menu-wrap"></div>
+          <ul className="side-menu-list">
+            <li><i className="fa-light fa-location-dot"></i>Address : <span>Amsterdam, 109-74</span></li>
+            <li><i className="fa-light fa-phone"></i>Phone : <a href="tel:+01569896654">+01 569 896 654</a></li>
+            <li><i className="fa-light fa-envelope"></i>Email : <a href="mailto:info@example.com">info@example.com</a>
             </li>
           </ul>
         </div>
--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
 // API Configuration
-const VITE_API_URL = process.env.VITE_API_URL || 'http://localhost:1337';
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL || 'http://localhost:1337';
 
-export const API_BASE_URL = `${VITE_API_URL}/api`;
+export const API_BASE_URL = `${REACT_APP_API_URL}/api`;
 
 export default API_BASE_URL;
